8dcdca34718961716b602d9c254c80759a76931d
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-30 15:59:04 +0100

    Added unit and mouse position

---
+++ Assets/Scripts/Tools/MousePosition.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Hex.Tools
+{
+    public class MousePosition : MonoBehaviour
+    {
+
+        [SerializeField] private LayerMask floorLayerMask;
+        private static MousePosition instance;
+        private void Awake() {
+            instance = this;
+        }
+
+        public static Vector3 GetPosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, instance.floorLayerMask);
+            return hit.point;
+        }
+    }
+}
