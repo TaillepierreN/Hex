94cd651f7de4bcbd1c6f83ae66a23cd41b4ceff6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-02-06 10:41:32 +0100

    added movement animation and rotation

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -5,8 +5,10 @@ namespace Hex.Units
 {
     public class Unit : MonoBehaviour
     {
+        [SerializeField] private Animator unitAnimator;
         private Vector3 targetPosition;
         private float moveSpeed = 4f;
+        private float rotateSpeed = 10f;
         private float stoppingDistance = .1f;
 
         private void Update()
@@ -15,6 +17,12 @@ namespace Hex.Units
             {
                 Vector3 moveDirection = (targetPosition - transform.position).normalized;
                 transform.position += moveDirection * moveSpeed * Time.deltaTime;
+                transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+                unitAnimator.SetBool("isWalking", true);
+            }
+            else
+            {
+                unitAnimator.SetBool("isWalking", false);
             }
             if (Input.GetMouseButtonDown(0))
             {
