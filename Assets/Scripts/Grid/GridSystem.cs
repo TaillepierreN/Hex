bc06f63fc2e5324ea13a3a6318bc9eebbff2242f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-02-25 11:39:23 +0100

    added a bit of doc

--- Assets/Scripts/Grid/GridSystem.cs
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -11,6 +11,12 @@ namespace Hex.Grid
 		private float _cellSize;
 		private GridObject[,] _gridObjectArray;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GridSystem"/> class with the specified width, height, and cell size.
+		/// </summary>
+		/// <param name="width">The width of the grid.</param>
+		/// <param name="height">The height of the grid.</param>
+		/// <param name="cellSize">The size of each cell in the grid.</param>
 		public GridSystem(int width, int height, float cellSize)
 		{
 			this._width = width;
@@ -23,16 +29,25 @@ namespace Hex.Grid
 				for (int z = 0; z < _height; z++)
 				{
 					GridPosition gridPosition = new GridPosition(x, z);
-					_gridObjectArray[x, z] = new GridObject( this, gridPosition);
+					_gridObjectArray[x, z] = new GridObject(this, gridPosition);
 				}
 			}
 		}
-
+		/// <summary>
+		/// Converts a grid position to a world position.
+		/// </summary>
+		/// <param name="gridPosition">The position in the grid to be converted.</param>
+		/// <returns>A Vector3 representing the world position corresponding to the given grid position.</returns>
 		public UnityEngine.Vector3 GetWorldPosition(GridPosition gridPosition)
 		{
 			return new UnityEngine.Vector3(gridPosition.x, 0, gridPosition.z) * _cellSize;
 		}
-
+		
+		/// <summary>
+		/// Converts a world position to a grid position.
+		/// </summary>
+		/// <param name="worldPosition">The world position to convert.</param>
+		/// <returns>A GridPosition representing the grid coordinates of the given world position.</returns>
 		public GridPosition GetGridPosition(UnityEngine.Vector3 worldPosition)
 		{
 			return new GridPosition(
@@ -60,6 +75,6 @@ namespace Hex.Grid
 		{
 			return _gridObjectArray[gridPosition.x, gridPosition.z];
 		}
-		
+
 	}
 }
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -2,54 +2,76 @@ using Hex.Grid;
 using Hex.Tools;
 using UnityEngine;
 
-namespace Hex.Units
+/// <summary>
+/// Represents a unit in the game, which can move and interact with the grid.
+/// </summary>
+public class Unit : MonoBehaviour
 {
-    public class Unit : MonoBehaviour
+
+    [SerializeField] private Animator _unitAnimator;
+
+
+    private Vector3 _targetPosition;
+    private GridPosition _gridPosition;
+
+    /// <summary>
+    /// The speed at which the unit moves.
+    /// </summary>
+    private float _moveSpeed = 4f;
+
+    /// <summary>
+    /// The speed at which the unit rotates.
+    /// </summary>
+    private float _rotateSpeed = 10f;
+
+    /// <summary>
+    /// The distance at which the unit stops moving towards the target position.
+    /// </summary>
+    private float _stoppingDistance = .1f;
+
+
+
+    private void Awake()
     {
-        [SerializeField] private Animator _unitAnimator;
+        _targetPosition = transform.position;
+    }
 
-        private Vector3 _targetPosition;
-        private GridPosition _gridPosition;
-        private float _moveSpeed = 4f;
-        private float _rotateSpeed = 10f;
-        private float _stoppingDistance = .1f;
+    private void Start()
+    {
+        _gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(_gridPosition, this);
+    }
 
-
-        private void Awake()
+    /// <summary>
+    /// Updates the unit's position, rotation, and grid position each frame.
+    /// </summary>
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, _targetPosition) > _stoppingDistance)
         {
-            _targetPosition = transform.position;
+            Vector3 moveDirection = (_targetPosition - transform.position).normalized;
+            transform.position += moveDirection * _moveSpeed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * _rotateSpeed);
+            _unitAnimator.SetBool("isWalking", true);
         }
-
-        private void Start()
+        else
         {
-            _gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
-            LevelGrid.Instance.AddUnitAtGridPosition(_gridPosition, this);
+            _unitAnimator.SetBool("isWalking", false);
         }
-        private void Update()
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != _gridPosition)
         {
-            if (Vector3.Distance(transform.position, _targetPosition) > _stoppingDistance)
-            {
-                Vector3 moveDirection = (_targetPosition - transform.position).normalized;
-                transform.position += moveDirection * _moveSpeed * Time.deltaTime;
-                transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * _rotateSpeed);
-                _unitAnimator.SetBool("isWalking", true);
-            }
-            else
-            {
-                _unitAnimator.SetBool("isWalking", false);
-            }
-            GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
-            if(newGridPosition != _gridPosition)
-            {
-                LevelGrid.Instance.UnitMovedGridPosition(_gridPosition, newGridPosition, this);
-                _gridPosition = newGridPosition;
-            }
-
+            LevelGrid.Instance.UnitMovedGridPosition(_gridPosition, newGridPosition, this);
+            _gridPosition = newGridPosition;
         }
-        public void Move(Vector3 targetPosition)
-        {
-            this._targetPosition = targetPosition;
-        }
+    }
 
+    /// <summary>
+    /// Moves the unit to the specified target position.
+    /// </summary>
+    /// <param name="targetPosition">The target position to move the unit to.</param>
+    public void Move(Vector3 targetPosition)
+    {
+        this._targetPosition = targetPosition;
     }
 }
